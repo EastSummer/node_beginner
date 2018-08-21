c86e5027cb0d50e98db6dca268138afb13248dcd
陈鹏飞 <pengfei.chen@sumscope.com>
2018-08-21 16:57:24 +0800

    update

--- imovie/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var port = process.env.PORT || 3000
-var app = express()
-
-app.set('views', '.views')
-app.set('view engine', 'jade')
-app.listen(port)
-
-console.log('imovie started on port ' + port)
